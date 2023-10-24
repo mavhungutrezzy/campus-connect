ba83ea0fe23e5e2ce9749a4b5b16cab6ec67963f
mavhungutrezzy <mavhungutrezzy@gmail.com>
2023-10-24 15:56:43 +0200

    🔨 refactor(models.py): update University model description field max length to 255 for better consistency 🔨 refactor(models.py): update ApplicationInformation model additional_information field to be optional (blank=True) 🔨 refactor(models.py): add contact_email and contact_phone fields to ApplicationInformation model for better contact information 🔨 refactor(models.py): add optional social media URLs (facebook_url, twitter_url, linkedin_url) to ApplicationInformation model 🔨 refactor(models.py): update Course model standardized_test_type field max length to 50 for better consistency

--- prospectuses/models.py
+++ prospectuses/models.py
@@ -6,22 +6,18 @@ from django.utils.text import slugify
 User = get_user_model()
 
 
+from django.db import models
+from django.utils.text import slugify
+
+
 class University(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True, blank=True)
-    description = models.TextField(max_length=25)
+    description = models.TextField(max_length=255)
     website = models.URLField(max_length=255)
     province = models.CharField(max_length=30)
     city = models.CharField(max_length=60)
     logo_url = models.URLField(max_length=255)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
-
-
-class ApplicationInformation(models.Model):
     application_fee_amount = models.DecimalField(decimal_places=2, max_digits=5)
     application_methods = (
         ("Online", "Apply Online"),
@@ -30,12 +26,20 @@ class ApplicationInformation(models.Model):
     application_method = models.CharField(
         max_length=20, choices=application_methods, default="Online"
     )
-
     application_url = models.URLField()
     application_open_date = models.DateField()
     application_close_date = models.DateField()
-    additional_information = RichTextField()
-    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    additional_information = RichTextField(blank=True)
+    contact_email = models.EmailField()
+    contact_phone = models.CharField(max_length=20)
+    facebook_url = models.URLField(max_length=255, blank=True, null=True)
+    twitter_url = models.URLField(max_length=255, blank=True, null=True)
+    linkedin_url = models.URLField(max_length=255, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
 
 class Faculty(models.Model):
@@ -57,5 +61,7 @@ class Course(models.Model):
     eligibility = RichTextField(null=True, blank=True)
     careers = models.JSONField(null=True, blank=True)
     admission_point_score = models.IntegerField(default=24)
+    standardized_test = models.BooleanField(default=False)
+    standardized_test_type = models.CharField(max_length=50, blank=True)
     faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
     university = models.ForeignKey(University, on_delete=models.CASCADE)
